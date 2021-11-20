9e409a4fc2d0db512f85153b470698f872acc9c7
Scott Vu <scottvu369@outlook.com>
2021-11-19 18:15:44 -0600

    Initial Commit

---
+++ Review HW 4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Review HW 4
+//
+//  Created by Scott Vu on 11/19/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
